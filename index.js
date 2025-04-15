e8d12055f59ee93feb8db1764d1d16f4855998a6
davide-delgiudice <dav.del.giudice@gmail.com>
2025-04-15 16:45:55 +0200

    [UPD] create if condition for empty field - Bonus Extra

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ console.log("Hello Boolean");
 
 const newPhrase = require('./phrase')
 
-const phrase = process.argv[2];
+const phrase = process.argv[2] ? process.argv[2] : "Inserisci una frase";
 
 console.log(newPhrase(phrase));
