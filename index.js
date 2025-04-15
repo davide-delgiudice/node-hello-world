f0ae70ac54882efe2f3551f2e5cfb76c07b8996f
davide-delgiudice <dav.del.giudice@gmail.com>
2025-04-15 16:40:48 +0200

    [UPD] add newPhrase with argv process

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 console.log("Hello Boolean");
 
-const newPhrase = require('./phrase');
+// const newPhrase = require('./phrase');
 
-console.log(newPhrase("Quanto è bello Dragon Age?"));
+// console.log(newPhrase("Quanto è bello Dragon Age?"));
+
+const newPhrase = require('./phrase')
+
+const phrase = process.argv[2];
+
+console.log(newPhrase(phrase));
